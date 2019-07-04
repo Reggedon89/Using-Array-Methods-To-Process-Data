656e91e2a4897c3592934830be2448a7cbd2575f
Geoff Hutson <geoff.hutson.89@gmail.com>
2019-07-03 21:02:30 -0700

    questions 1-4 are finished

---
+++ main.js
@@ -0,0 +1,49 @@
+
+// Question 1
+
+const sum = items.reduce(function(a, b)  {
+    return a + b.price
+}, 0) / items.length
+
+const ans = `The average price is $${sum.toFixed(2)}`
+
+document.querySelector("#answer1").innerHTML = ans
+
+
+//Question 2
+
+var range = items.filter((prices) => {
+    return prices.price <=18 && prices.price >= 14 
+}).map((name) => {
+    return name.title
+}).slice(1,4)
+
+document.querySelector("#answer2").innerHTML = range
+//Question 3
+var currency = items.filter((x) => {
+      return  x.currency_code === "GBP"  
+ }).map((y) => {
+     let name = y.title
+     let price = y.price
+     return `${name} costs ${price}`
+ })
+
+ document.querySelector("#answer3").innerHTML = currency.toString()
+
+//Question 4
+ var wood = items.filter((x) => {
+     let getWood = x.materials
+     for (let i = 0; i < getWood.length; i++) {
+         if (getWood[i] === "wood") {
+             return getWood
+         }
+     }
+     return 
+ }).map((y) => {
+     let name = y.title;
+      return `${name} is made of wood`   
+     })
+
+
+console.log(wood)
+
